68ad2f9d8e7c42dc9db10455d6fedf1b5203d5c7
Ben Wintraub <benw24@downtownschoolseattle.org>
2023-08-13 12:21:43 -0700

    convert to onnx

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-console.log("loading")
-import { pipeline } from 'https://cdn.jsdelivr.net/npm/@xenova/transformers@2.3.0';
-console.log("loaded transformers")
+let start = Date.now()
+
+import { pipeline, env } from 'https://cdn.jsdelivr.net/npm/@xenova/transformers@2.5.1';
 
 const input1 = document.getElementById('input1');
 const input2 = document.getElementById('input2');
 const generateButton = document.getElementById('generate-button');
 const output = document.getElementById('output');
 
+env.localModelPath = "/models/"
+env.allowRemoteModels = false;
+// env.backends.onnx.wasm.wasmPaths = '/wasm/';
+
+
+
 const generateEmbeddings = await pipeline(
   'feature-extraction',
-  'thenlper/gte-small'
+  'thenlper/gte-small',
+  {quantized: true}
 );
 
-console.log("loaded transformers")
+let timeTaken = Date.now() - start;
+
+console.log("loaded transformers in " + timeTaken)
 
 generateButton.disabled = false;
 
